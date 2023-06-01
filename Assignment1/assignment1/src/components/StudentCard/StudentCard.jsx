277c288f827fd6e6e995a61ae3d7ffbcd7d1d963
Jing Jin <jingjin496@gmail.com>
2023-05-31 23:21:06 -0700

    assignment1

--- Assignment1/assignment1/src/components/StudentCard/StudentCard.jsx
+++ Assignment1/assignment1/src/components/StudentCard/StudentCard.jsx
@@ -1,22 +1,25 @@
 import './StudentCard.css';
 const StudentCard = ({ data }) =>{
     return (
-        <div className='card-container'>
-            <div className='card-header'>
-                <img src={data.avatar} alt=""/>
-            </div>
-            <div className='card-body'>
-                <div>
-                    <span>Name:</span>
-                    <span> {data.first_name},{data.last_name}</span>
+        <div className='wapper'>
+            <div className='card-container'>
+                <div className='card-header'>
+                    <img src={data.avatar} alt="" width="100px" height="150px" />
                 </div>
-                <div>
-                    <span>Email: </span>
-                    <span> {data.email}</span>
+                <div className='card-body' >
+                    <div className='nested'>
+                        <span className='data-titile'>Name:</span>
+                        <br></br>
+                        <span> {data.first_name},{data.last_name}</span>
+                    </div>
+                    <div className='nested'>
+                        <span className='data-titile'>Email: </span>
+                        <span> {data.email}</span>
+                    </div>
                 </div>
-            </div>
-           
             
+                
+            </div>
         </div>
     )
 }
